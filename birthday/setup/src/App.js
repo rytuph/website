2a99aaa0e47bf371e0de5e611b9a9809b1b5a9bb
Ryan Zhang <ryanzhang50@gmail.com>
2022-12-01 02:13:14 -0500

    restarted tutorial

--- birthday/setup/src/App.js
+++ birthday/setup/src/App.js
@@ -2,15 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return(
-    <main>
-      <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
-        <button onClick={()=> console.log('you clicked me')}> clear all</button>
-      </section>
-    </main>
-  );
+  return <h2>reminder project setup</h2>;
 }
 
 export default App;
--- birthday/setup/src/data.js
+++ birthday/setup/src/data.js
@@ -1,77 +1,35 @@
 export default [
   {
     id: 1,
-    name: 'Isabel Yang',
+    name: 'Bertie Yates',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Simon Yang',
+    name: 'Hester Hogan',
     age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Leon Zhao',
+    name: 'Larry Little',
     age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Jinyi Lu',
+    name: 'Sean Walsh',
     age: 34,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
-    name: 'Raymond Liu',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-  {
-    id: 6,
-    name: 'Alex Li',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-  {
-    id: 7,
-    name: 'Ethan Guan',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-  {
-    id: 8,
-    name: 'Sanjay Ramesh',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-  {
-    id: 9,
-    name: 'Laura Wang',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-  {
-    id: 10,
-    name: 'Adora Chen',
-    age: 29,
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-  },
-  {
-    id: 11,
-    name: 'Tina Geng',
+    name: 'Lola Gardner',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
