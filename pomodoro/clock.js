65911ba9b4aa2087df7ea13e2b92170ea5574954
Ryan Zhang <ryanzhang50@gmail.com>
2022-12-02 20:03:54 -0500

    premature initiation. different buttons set different times but the timer displays all of them instead of resetting

--- pomodoro/clock.js
+++ pomodoro/clock.js
@@ -9,26 +9,26 @@ let clock = document.getElementById('clock');
 let playButton = document.getElementById('playbutton');
 
 
-// class to create timer
-class timer {
-    // setup timer values?
-    constructor({selector, targetDate, backgroundColor = null, foregroundcolor = null}){
+// class to create a countdown timer
+class CountdownTimer {
+    // setup timer values
+    constructor({ selector, targetDate, backgroundColor = null, foregroundColor = null }) {
         this.selector = selector;
         this.targetDate = targetDate;
         this.backgroundColor = backgroundColor;
-        this.foregroundcolor = foregroundcolor;
+        this.foregroundColor = foregroundColor;
 
-
-        // grabs divs on frontend using supplied selector ID
-        this.refs = {//object
+        // grab divs on frontend using supplied selector ID
+        this.refs = {
+            days: document.querySelector(`${this.selector} [data-value="days"]`),
             hours: document.querySelector(`${this.selector} [data-value="hours"]`),
-            minutes: document.querySelector(`${this.selector} [data-value="minutes"]`),//keys and values
-            seconds: document.querySelector(`${this.selector} [data-value="seconds"]`)
+            mins: document.querySelector(`${this.selector} [data-value="minutes"]`),
+            secs: document.querySelector(`${this.selector} [data-value="seconds"]`),
         };
     }
-    
+
     getTimeRemaining(endtime) {
-        const total = Date.parse(endtime) = Date.parse(new Date());
+        const total = Date.parse(endtime) - Date.parse(new Date());
         const days = Math.floor(total / (1000 * 60 * 60 * 24));
         const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
         const mins = Math.floor((total / 1000 / 60) % 60);
@@ -41,12 +41,28 @@ class timer {
             secs,
         };
     }
-    
-    updateTimer({days, hours, mins, secs}) {
+
+    updateTimer({ days, hours, mins, secs }) {
         this.refs.days.textContent = days;
         this.refs.hours.textContent = hours;
         this.refs.mins.textContent = mins;
         this.refs.secs.textContent = secs;
+    }
+
+    updateColors() {
+        if (this.backgroundColor != null) {
+            this.refs.days.style.background = this.backgroundColor;
+            this.refs.hours.style.background = this.backgroundColor;
+            this.refs.mins.style.background = this.backgroundColor;
+            this.refs.secs.style.background = this.backgroundColor;
+        }
+
+        if (this.foregroundColor != null) {
+            this.refs.days.style.color = this.foregroundColor;
+            this.refs.hours.style.color = this.foregroundColor;
+            this.refs.mins.style.color = this.foregroundColor;
+            this.refs.secs.style.color = this.foregroundColor;
+        }
     }
 
     startTimer() {
@@ -59,20 +75,3 @@ class timer {
         }, 1000);
     }
 }
-// setting buttons to their functions
-
-pomodoroButton.addEventListener('click', function(){
-
-})
-
-shortButton.addEventListener('click', function(){
-
-})
-
-longButton.addEventListener('click', function(){
-
-})
-
-playButton = addEventListener('click', function(){
-
-})
--- pomodoro/init.js
+++ pomodoro/init.js
@@ -1,6 +1,35 @@
-const timer = new CountdownTimer({
-    selector: "#clock",
-    targetDate: new Date(new Date().getTime()+(25*60*1000))
-});
 
-timer.startTimer();
+
+
+
+// setting buttons to their functions
+
+while (1){
+    pomodoroButton.addEventListener('click', function(){
+        const timer = new CountdownTimer({
+            selector: "#clock",
+            targetDate: new Date(new Date().getTime()+(25*60*1000))
+        });
+    
+    })
+    
+    shortButton.addEventListener('click', function(){
+        const timer = new CountdownTimer({
+            selector: "#clock",
+            targetDate: new Date(new Date().getTime()+(5*60*1000))
+        });
+    
+    })
+    
+    longButton.addEventListener('click', function(){
+        const timer = new CountdownTimer({
+            selector: "#clock",
+            targetDate: new Date(new Date().getTime()+(15*60*1000))
+        });
+    
+    })
+    
+    playButton = addEventListener('click', function(){
+        timer.startTimer();
+    })
+}
