1f0dc900ddca1310a75b874e96dddbea80ec088b
Ryan Zhang <ryanzhang50@gmail.com>
2022-12-04 22:34:06 -0500

    fixed bug caused by while loop started second branch

--- pomodoro2/js/init.js
+++ pomodoro2/js/init.js
@@ -1,13 +1,10 @@
-// setting buttons to their functions
-// if pomodoro, short, or long break are clicked, timer resets
-
 
 pomodoroButton.addEventListener('click', function(){
     const timer = new CountdownTimer({
         selector: "#clock",
         targetDate: new Date(new Date().getTime()+(25*60*1000))
     });
-
+    timer.startTimer();
 })
 
 shortButton.addEventListener('click', function(){
@@ -15,7 +12,7 @@ shortButton.addEventListener('click', function(){
         selector: "#clock",
         targetDate: new Date(new Date().getTime()+(5*60*1000))
     });
-
+    timer.startTimer();
 })
 
 longButton.addEventListener('click', function(){
@@ -23,10 +20,9 @@ longButton.addEventListener('click', function(){
         selector: "#clock",
         targetDate: new Date(new Date().getTime()+(15*60*1000))
     });
-
+    timer.startTimer();
 })
 
 playButton = addEventListener('click', function(){
     timer.startTimer();
 })
-
