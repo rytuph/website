f3c914e136f9f864a2eae470a2df63be3cbb6240
Ryan Zhang <ryanzhang50@gmail.com>
2022-12-20 22:13:39 -0500

    added pause function

--- pomodoro2/init.js
+++ pomodoro2/init.js
@@ -28,9 +28,36 @@ longButton.addEventListener('click', function(){
     timer.startTimer();
 })
 
-play_pause.addEventListener('click', function(){
+// play_pause.addEventListener('click', function(){
     
-    playpause();
+//     playpause();
+// })
+
+play_pause.addEventListener('click', function(){
+    console.log(typeof interval);
+    if (typeof interval == 'number') {
+	// is there better way to judge if interval is running?
+	// timer.cancelPreviousTimer();
+	clearInterval(interval);
+	interval = null;// so that interval is not number anymore
+    } else {
+    
+        let seconds = parseInt(document.querySelector(`[data-value="seconds"]`).textContent);
+        let minutes= parseInt(document.querySelector(`[data-value="minutes"]`).textContent);
+        let hours= parseInt(document.querySelector(`[data-value="hours"]`).textContent);
+        let days= parseInt(document.querySelector(`[data-value="days"]`).textContent);
+	// use hours and days below
+	if (isNaN(seconds)) {
+		alert('not started yet');
+	} else {
+        const timer = new CountdownTimer({
+            selector: "#clock",
+            targetDate: new Date(new Date().getTime()+(seconds * 1000 + minutes * 60000))
+        });
+        timer.startTimer();
+	}
+    }
 })
+
 
 // timer.startTimer(); 
